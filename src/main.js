4fa6502e228059b7d611660db20f0455c52238e9
Aleksander <aleksander.0705@gmail.com>
2024-04-25 17:21:01 +0300

    create repo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/about-me';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
+import './js/menu';
+import './js/modal';
