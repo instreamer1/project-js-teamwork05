57ee05cbb037293ed0494883a6b78b3dfea975cc
Tatianadardik44 <dardiktatiana@gmail.com>
2024-04-28 13:50:42 +0200

    add-padding-bottom

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 const accordions = Array.from(document.querySelectorAll('.accordion-container'));
-new Accordion(accordions, {duration: 400,
+new Accordion(accordions, {duration: 800,
     showMultiple: true
   });
