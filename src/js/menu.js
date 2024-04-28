455467b124d6c3e5b550063c0238c86011e31d71
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-04-28 18:16:48 +0300

    Js finished

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,21 @@
+// Відкриття Меню
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
-
 menuToggle.addEventListener('click', toggleMenu);
 
+
+// Відкриття модального вікна
+const backdrop = document.querySelector('.backdrop');
+const burgerIcon = document.querySelector('.burger-icon');
+burgerIcon.addEventListener('click', openMenu);
+
+// Закриття модального вікна
+const closeButton = document.querySelector('.xclose-btn-mob');
+closeButton.addEventListener('click', closeMenu);
+
+
+
+// Функція для відкриття/зактирря Меню
 function toggleMenu () {
     if (menu.style.display === 'none') {
         menu.style.display = 'block';
@@ -13,3 +26,15 @@ function toggleMenu () {
 }
 
 
+// Функція для відкриття модального вікна
+function openMenu() {
+    backdrop.classList.add('is-open');
+    console.log('isOpen');
+}
+
+
+// Функція для закриття модального вікна
+function closeMenu() {
+    backdrop.classList.remove('is-open');
+    console.log('isClose');
+}
