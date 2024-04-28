1412bbaf83c7ecfd03c8792eb709d9005a737289
VladyslavBihun <bihunvv@gmail.com>
2024-04-28 22:29:34 +0300

    library added and configured

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,31 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Keyboard, Mousewheel, Navigation} from 'swiper/modules';
 
+const swiper = new Swiper('.swiper-container', {
+
+  modules: [Keyboard, Mousewheel, Navigation],
+  speed: 500,
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 16,  
+
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    enabled: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      spaceBetween: 32,
+    },
+  },
+
+});
