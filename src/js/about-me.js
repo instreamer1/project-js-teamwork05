bbba70abdc6748474bec0bd5a1289c5e99f89246
nectakin <kvs171005@gmail.com>
2024-04-28 22:43:44 +0300

    upd js
    зроблено функціонал accordion. swiper у процессі. svg та img будуть виправлені коли вже все працюватиме.
    
    єдина проблема-ніяк не виходить поставити у ряд з іншими заголовок About me і його стрілочку. вони зміщені вліво. маржинами і паддінгами не виправляється.  тому поки залишила так. якщо у вас буде час це переглянути і виправити, напишіть, будь ласка

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,2 +1,21 @@
 // import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
+// import Accordion from 'https://cdn.skypack.dev/accordion-js';
+import Accordion from 'https://cdn.skypack.dev/accordion-js';
+// import 'https://cdn.skypack.dev/accordion-js/dist/accordion.min.css';
+
+
+
+const accordions = Array.from(document.querySelectorAll('.accordion-container'));
+new Accordion(accordions, {
+    duration: 400,
+    showMultiple: true,
+    onOpen: function(currentElement) {
+    console.log(currentElement);
+  }
+});
+
+
+
+
+/////////////////////////////
