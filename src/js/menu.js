32e898a39b1876dd5d1364c8fa34b76be5651b43
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-04-28 15:46:24 +0300

    cursor pointer + js start

---
+++ src/js/menu.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', toggleMenu);
+
+function toggleMenu () {
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
