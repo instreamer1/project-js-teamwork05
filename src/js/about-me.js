ec6710fed33caae5f6428260f1f1943c3283cf5c
nectakin <kvs171005@gmail.com>
2024-04-29 19:22:27 +0300

    new

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,2 +1,15 @@
 // import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
+import 'accordion-js/dist/accordion.min.css';
+function initializeAccordion() {
+    const accordions = document.querySelectorAll('.accordion-container');
+    accordions.forEach(acc => {
+        new Accordion(acc, {
+            duration: 400,
+            showMultiple: true,
+            onOpen: function(currentElement) {
+                console.log(currentElement);
+            }
+        });
+    });
+}
