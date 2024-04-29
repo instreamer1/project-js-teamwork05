6feef2542ad337fc58e9cd510116f40927c142e5
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-04-29 13:18:07 +0300

    burger-modal close aftern choise

--- src/js/menu.js
+++ src/js/menu.js
@@ -12,8 +12,10 @@ const burgerIcon = document.querySelector('.burger-icon');
 burgerIcon.addEventListener('click', openMenu);
 
 // Закриття модального вікна
+const modalMenu = document.querySelector('.mobile-menu-list');
 const closeButton = document.querySelector('.xclose-btn-mob');
 closeButton.addEventListener('click', closeMenu);
+modalMenu.addEventListener('click', closeMenu);
 
 // Функція для відкриття/зактирря Меню
 function toggleMenu() {
